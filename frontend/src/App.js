63762b463c74fa4d084e3d180b0a7573682a399c
Mirjami Laiho <mirjami.laiho@tuni.fi>
2023-01-16 21:48:20 +0200

    Add function for fetching all journey data to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,57 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import DataTable from "./components/Table";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([
+    {
+      departureTime: "-",
+      departureStation: "-",
+      returnTime: "-",
+      returnStation: "-",
+      duration: "-",
+      distance: "-",
+    },
+  ]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getJourneyData();
+  }, []);
+
+  const getJourneyData = () => {
+    let tempArr = [];
+
+    axios
+      .get("http://localhost:8080/journey_data")
+      .then((result) => {
+        for (let i = 0; i < result.data.length; i++) {
+          tempArr.push({
+            departureTime: result.data[i].departure_date,
+            departureStation: result.data[i].departure_station_name,
+            returnTime: result.data[i].return_date,
+            returnStation: result.data[i].return_station_name,
+            duration: result.data[i].duration,
+            distance: result.data[i].covered_distance,
+          });
+        }
+        setData(tempArr);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
   return (
     <div className="App">
-      <p>New App</p>
+      <DataTable data={data} />
     </div>
   );
-}
+};
 
 export default App;
